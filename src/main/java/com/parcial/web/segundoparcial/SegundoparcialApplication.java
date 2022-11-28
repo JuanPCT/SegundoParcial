378b15949911870265616e1579685e5c134a12e9
Juan Pablo Correa Tarazona <89850618+JuanPCT@users.noreply.github.com>
2022-11-28 10:23:05 -0500

    Commit inicial

---
+++ src/main/java/com/parcial/web/segundoparcial/SegundoparcialApplication.java
@@ -0,0 +1,13 @@
+package com.parcial.web.segundoparcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegundoparcialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegundoparcialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parcial/web/segundoparcial/SegundoparcialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parcial.web.segundoparcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegundoparcialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
